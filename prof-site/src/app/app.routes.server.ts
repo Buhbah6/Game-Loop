83832dd28141b0efb5c177a4171a352e24de32c4
Tony Nadeau <tonynadeau03@gmail.com>
2025-09-28 11:51:58 -0400

    Fixed build issues

--- prof-site/src/app/app.routes.server.ts
+++ prof-site/src/app/app.routes.server.ts
@@ -2,7 +2,43 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'search',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'articles',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tutorials',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'videos',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'articles/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'tutorials/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'videos/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
