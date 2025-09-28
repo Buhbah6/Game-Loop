81eba9943cb033555eaacf177752a1a47c2b4d5e
Tony Nadeau <tonynadeau03@gmail.com>
2025-09-28 12:11:09 -0400

    Fixed environment config

--- prof-site/src/app/services/strapi.service.ts
+++ prof-site/src/app/services/strapi.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment.ts';
+import { environment } from '@environments/environment';
 import { 
   StrapiResponse, 
   Article, 
