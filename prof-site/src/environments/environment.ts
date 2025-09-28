8a4b9bd3b96396f539cef1ffe28fbcac40b12405
Tony Nadeau <tonynadeau03@gmail.com>
2025-09-28 12:16:34 -0400

    Fixed environment config

---
+++ prof-site/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  strapi: {
+    url: 'http://localhost:1337', // Default Strapi URL
+    apiToken: 'your-strapi-api-token-here' // Replace with your actual Strapi API token
+  }
+};
